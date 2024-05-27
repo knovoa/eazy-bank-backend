8e42c9e14132a82952de9959f7018e6587c6b9b0
Kevin Novoa <kevnovoa94.kn@gmail.com>
2024-05-26 22:58:04 -0500

    chore: Initial commit

---
+++ src/main/java/com/examples/app/basics/SpringSecurityBasicsApplication.java
@@ -0,0 +1,12 @@
+package com.examples.app.basics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityBasicsApplication.class, args);
+	}
+}
---
+++ src/main/java/com/examples/app/basics/controller/WelcomeController.java
@@ -0,0 +1,16 @@
+package com.examples.app.basics.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class WelcomeController {
+
+	@GetMapping("/")
+	public String sayHello() {
+		log.info("passing welcome controller >> sayHello() method");
+		return "Welcome to Spring Application with out security";
+	}
+}

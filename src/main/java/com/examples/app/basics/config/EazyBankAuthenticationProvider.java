4fe1b57332b636dc29516ea10bc5a1ae0dff4ce8
Kevin Novoa <kevnovoa94.kn@gmail.com>
2024-11-24 19:53:10 -0500

    chore: add custom authentication provider

---
+++ src/main/java/com/examples/app/basics/config/EazyBankAuthenticationProvider.java
@@ -0,0 +1,46 @@
+package com.examples.app.basics.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class EazyBankAuthenticationProvider implements AuthenticationProvider {
+
+  private final UserDetailsService userDetailsService;
+  private final PasswordEncoder passwordEncoder;
+
+  /**
+   * @param authentication {@link Authentication}
+   * @return {@link Authentication}
+   * @throws AuthenticationException when authentication fails
+   */
+  @Override
+  public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+	String username = authentication.getName();
+	String pwd = authentication.getCredentials().toString();
+	UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+	if (passwordEncoder.matches(pwd, userDetails.getPassword())) {
+	  return new UsernamePasswordAuthenticationToken(username, pwd, userDetails.getAuthorities());
+	} else {
+	  throw new BadCredentialsException("Invalid password!!!");
+	}
+  }
+
+  /**
+   * @param authentication {@link Class}
+   * @return {@link boolean}
+   */
+  @Override
+  public boolean supports(Class<?> authentication) {
+	return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
+  }
+}
